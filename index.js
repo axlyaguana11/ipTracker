019f0943dceb931373b5898424b0b75b3077e5de
axl <axl9809@gmail.com>
2021-04-04 23:48:58 -0500

    index.js added

---
+++ index.js
@@ -0,0 +1,14 @@
+const URL = 'https://geo.ipify.org/api/v1?apiKey='
+
+const apiKey = 'at_3L0043xoCy2KqnmovvRcZHmrREJbW'
+
+const numberList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+const mailSymbol = ['@']
+
+
+const userInput = document.querySelector('#address') //Not the value, but the element
+
+const searchSubmit = document.querySelector('#submit')
+
+console.log(userInput)
+
